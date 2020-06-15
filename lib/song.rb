2983ac4aaee6f0e544bda26c032a5db42db41c04
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-15 19:41:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,7 +58,7 @@ attr_reader :artist, :genre
     end
 
     def self.create_from_filename(filename)
-      new_from_filename(filename).tap{ |s| s.save }
+      new_from_filename(filename).tap{|s| s.save}
     end
     
 end
