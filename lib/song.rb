78c69be9778ad132b31f11f90a14bbdfec28c170
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-15 18:55:32 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+class Song
+  
+  @@all = []
+  
+attr_accessor :name
+attr_reader :artist, :genre
+  
+    def initialize(name)
+      @name = name
+    end
+    
+    def artist=(artist)
+     @artist = artist
+     artist.add_song(self)
+    end
+
+    def genre=(genre)
+     @genre = genre
+      genre.songs << self unless genre.songs.include?(self)
+    end
+  
+    def self.all
+      @@all
+    end
+    
+    def save
+    self.class.all << self
+    end
+    
+    def destroy_all
+      all.clear
+    end
+    
+    def self.create(name)
+     song = new(name)
+     song.save
+     song
+    end
+    
+end
