e524d33b5fdbdd535e34e8e2788dd94731e7cf07
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-15 19:53:16 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,7 +1,6 @@
 class MusicLibraryController
-
   def initialize(path = "./db/mp3s")
-  Music_Importer.new(path).import
+    MusicImporter.new(path).import
   end
 
   def call
@@ -37,45 +36,47 @@ class MusicLibraryController
     end
   end
 
-    def list_songs
+  def list_songs
     Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
       puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
     end
   end
 
-    def list_artists
+  def list_artists
     Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
       puts "#{i}. #{a.name}"
     end
   end
 
-    def list_genres
+  def list_genres
     Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
       puts "#{i}. #{g.name}"
     end
   end
 
-    def list_songs_by_artist
+  def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.strip
-      if artist = Artist.find_by_name(input)
+
+    if artist = Artist.find_by_name(input)
       artist.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
         puts "#{i}. #{s.name} - #{s.genre.name}"
       end
     end
   end
 
-    def list_songs_by_genre
+  def list_songs_by_genre
     puts "Please enter the name of a genre:"
     input = gets.strip
-      if genre = Genre.find_by_name(input)
+
+    if genre = Genre.find_by_name(input)
       genre.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
         puts "#{i}. #{s.artist.name} - #{s.name}"
       end
     end
   end
 
-    def play_song
+  def play_song
     puts "Which song number would you like to play?"
 
     input = gets.strip.to_i
@@ -85,6 +86,4 @@ class MusicLibraryController
 
     puts "Playing #{song.name} by #{song.artist.name}" if song
   end
-
-
 end
