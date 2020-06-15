5ca093d4da47b5234b2b7f5d0a9dd2f76d451ba7
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-15 18:58:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
 attr_accessor :name
 attr_reader :artist, :genre
   
-    def initialize(name, genre, )
+    def initialize(name, artist, genre)
       @name = name
       self.artist = artist if artist
       self.genre = genre if genre
