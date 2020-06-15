767f5b0f010362168d81f3e34ecca0c5b44550e7
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-15 19:39:20 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -2,9 +2,16 @@ class MusicImporter
 
 attr_reader :path
 
-def initialize(path)
+ def initialize(path)
   @path = path
-  
-end
+ end
+
+ def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+ end
+
+ def import
+    files.each{ |f| Song.create_from_filename(f) }
+  end
 
 end
